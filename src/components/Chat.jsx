00819be17e28ca2f84a99a5c9549750fae5afaee
Jaweria321 <jaweria.ehsan123@gmail.com>
2024-04-21 01:46:03 +0500

    working on single chat

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -5,14 +5,68 @@ function Chat() {
     const messages = [
         {
             id: 1,
-            avatar:'', 
-            contactTitle:'',
-            message:'',
+            avatar:'images/avatar-2.png', 
+            contactTitle:'Sophie Okonedo',
+            message:'John contacted you with Sophie Okenodo',
             imageMessage: '',
             isSender: false,
             emoji: '',
-            time:'',
+            time:'9:30 AM',
 
+        },
+        {
+            id: 2,
+            avatar:'images/profile-2.png', 
+            contactTitle:'Sophie Okonedo',
+            message:'Hi Alex! Have you heard about the upcoming virtual UX design event next month?',
+            imageMessage: '',
+            isSender: false,
+            emoji: '',
+            time:'9:30 AM',
+
+        },
+        {
+            id: 3,
+            avatar:'images/profile-2.png', 
+            contactTitle:'You',
+            message:'Hey Ali! Yes, I got tickets. Super excited! 🎶',
+            imageMessage: '',
+            isSender: true,
+            emoji: '',
+            time:'9:30 AM',
+
+        },
+        {
+            id: 4,
+            avatar:'images/profile-2.png', 
+            contactTitle:'Sophie Okonedo',
+            message:'Hey Samir! Are you going to the concert tomorrow night?',
+            imageMessage: '',
+            isSender: false,
+            emoji: '&#129395;',
+            time:'9:30 AM',
+
+        },
+        {
+            id: 5,
+            avatar:'images/profile-2.png', 
+            contactTitle:'You',
+            message:'Hey Sophie! Yes, I got tickets. Super excited! 🎶',
+            imageMessage: 'images/chat-img.png',
+            isSender: true,
+            emoji: '',
+            time:'9:30 AM',
+
+        },
+        {
+            id: 6,
+            avatar:'images/profile-1.png', 
+            contactTitle:'',
+            message:'That nice Samir see you there',
+            imageMessage: '',
+            isSender: false,
+            emoji: '',
+            time:'7 min ago',
         }
     ]
   return (
@@ -20,6 +74,7 @@ function Chat() {
         {
             messages.map(message => 
                 <ChatMessage 
+                key={message.id}
                 avatar={message.avatar}  
                 contactTitle={message.contactTitle}  
                 message={message.message} 
--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -2,21 +2,21 @@ import React from 'react'
 
 function ChatMessage({avatar, contactTitle, message, imageMessage, isSender, emoji, time}) {
   return (
-    <div className='flex gap-x-2' dir={false ? 'rtl' : 'ltr'}>
+    <div className='flex gap-x-2' dir={isSender ? 'rtl' : 'ltr'}>
         <div>
-          <img src="images/profile-1.png" alt="" className="rounded-full w-9 h-9" />
+          <img src={Image} alt="" className="rounded-full w-9 h-9" />
         </div>
         <div>
           <p className='flex text-sm'>
-              <span className='font-extrabold me-2'>Sophie Okonedo</span>
-              <span className='font-normal' dir='ltr'>9:30 AM</span>
+              <span className='font-extrabold me-2'>{contactTitle}</span>
+              <span className='font-normal' dir='ltr'>{time}</span>
           </p>
           <div className='flex items-center'>
             <div>
-              { false ? <img src="images/chat-img.png" alt="" className='mb-1' /> :
+              { imageMessage ? <img src="images/chat-img.png" alt="" className='mb-1' /> :
                <p className='max-w-[456px] bg-[#DFE2E8] py-1 px-3 rounded-lg rounded-ss-sm relative'>
-                Hi Alex! Have you heard about the upcoming virtual UX design event next month?
-                <span className='absolute end-4 bottom-[-20px] p-1 rounded-full bg-white'>&#129395;</span>
+                {message}
+                <span className='absolute end-4 bottom-[-20px] p-1 rounded-full bg-white'>{emoji}</span>
               </p> }
             </div>
             <ul className='flex items-center gap-2'>
